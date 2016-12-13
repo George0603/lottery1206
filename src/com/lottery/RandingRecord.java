c34c98b4ec2b6f8d892afa63a8b89ea6b4a03df6
George0603 <1152614219@qq.com>
2016-12-13 14:44:23 +0800

    添加算法类和数字未出现次数排名记录

---
+++ src/com/lottery/RandingRecord.java
@@ -0,0 +1,28 @@
+package com.lottery;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RandingRecord {
+
+	public static Map<String, String> randingRecord = new HashMap<String, String>();
+	
+	{
+		randingRecord.put("2016145", "2 9 11 12 19 30");
+		randingRecord.put("2016144", "5 9 19 20 24 33");
+		randingRecord.put("2016143", "9 12 16 17 27 32");
+		randingRecord.put("2016142", "3 14 22 23 31 32");
+		randingRecord.put("2016141", "4 5 9 18 22 31");
+		randingRecord.put("2016140", "3 6 16 22 28 31");
+		randingRecord.put("2016139", "1 8 9 22 23 33");
+		randingRecord.put("2016138", "1 2 9 13 24 25");
+		randingRecord.put("2016137", "2 3 5 7 17 30");
+		randingRecord.put("2016136", "5 6 23 28 30 32");
+		randingRecord.put("2016135", "4 6 17 20 32 33");
+		randingRecord.put("2016134", "1 9 12 21 25 33");
+		randingRecord.put("2016133", "1 2 4 19 22 32");
+		randingRecord.put("2016132", "1 18 20 27 31 33");
+		randingRecord.put("2016131", "3 4 14 15 19 32");
+		randingRecord.put("2016130", "3 7 8 25 27 31");
+	}
+}
