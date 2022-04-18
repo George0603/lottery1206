e94b4f58342333c9ea683f3a69cd24bda12b54d9
George0603 <1152614219@qq.com>
2022-04-18 10:57:15 +0800

    req #0418

--- src/com/main/RunMain.java
+++ src/com/main/RunMain.java
@@ -13,7 +13,7 @@ public class RunMain {
 	private static Logger logger = Logger.getLogger(RunMain.class);
 
 	public static void main(String[] args) {
-		twoColor();
+		sevenColor();
 		printInfo();
 	}
 
@@ -23,7 +23,7 @@ public class RunMain {
 		// 打印排名情况
 		// PrintUtils.printRanking();
 		// PrintUtils.printResult7();
-		// PrintUtils.pringDantuo();
+		PrintUtils.pringDantuo();
 	}
 
 	public static void twoColor() {
