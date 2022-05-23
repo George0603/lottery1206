657a0fc1d523600ea14f657f492b53a601162095
George0603 <1152614219@qq.com>
2022-05-23 20:55:08 +0800

    req #0523

--- src/com/lottery/sevencolor/Algorithm.java
+++ src/com/lottery/sevencolor/Algorithm.java
@@ -27,7 +27,7 @@ public class Algorithm {
 
 	public static final int SUM_DIFF = 30;
 
-	public static final List<Integer> EXCEPTLIST = Arrays.asList(5, 28);
+	public static final List<Integer> EXCEPTLIST = Arrays.asList(4, 7, 30);
 
 	private Algorithm() {
 		throw new IllegalStateException("Algorithm class");
